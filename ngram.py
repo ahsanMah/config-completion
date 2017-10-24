98964b2054c73fda91cd423fd9a332663020be7c
ahsanMah <mahmood.ahsan@outlook.com>
2017-10-24 06:27:18 -0400

    Worked on evaluation

--- ngram.py
+++ ngram.py
@@ -22,16 +22,16 @@ def train_ngram(train_set):
 	trigram_scores = tri_finder.score_ngrams(TrigramAssocMeasures.likelihood_ratio)
 
 
-	bigram_scores = sorted(bigram_scores, key=lambda item: item[1],reverse=True)
-	trigram_scores = sorted(trigram_scores, key=lambda item: item[1],reverse=True)
+	# bigram_scores = sorted(bigram_scores, key=lambda item: item[1],reverse=True)
+	# trigram_scores = sorted(trigram_scores, key=lambda item: item[1],reverse=True)
 
-	bigram_scores = bigram_scores[:5]
-	trigram_scores = trigram_scores[:5]
+	# bigram_scores = bigram_scores[:5]
+	# trigram_scores = trigram_scores[:5]
 
-	for result in bigram_scores: print result
-	for result in trigram_scores: print result
+	# for result in bigram_scores: print result
+	# for result in trigram_scores: print result
 
-	return
+	return bigram_scores
 
 def getTokens(dirname):
 	dirname = os.path.expanduser(dirname) 
@@ -51,14 +51,54 @@ def validate():
 	dirname = "~/arc/configs/examples"
 	train_list = getTokens(dirname)
 	split_set = LeaveOneOut().split(train_list)
+	# total_runs = len(split_set)
 	for train, test in split_set:
 		print("Train: %s, Test: %s" % (train, test))
 		train_set = []
 		for _idx in train:
 			train_set += train_list[_idx]
-		train_ngram(train_set)
-		break
-		
+		model = train_ngram(train_set)
+		model = create_mapping(model)
+		#Get accuracy
+		test_set = train_list[test[0]]
+		run_score = score(model,test_set)
+		print "Score: " + str(run_score)
+
+def score(model, test_set):
+	test_text = [token for token in test_set if "!" not in token]
+	bigram_list = list(bigrams(test_text))
+
+	total_bigrams = len(bigram_list)
+	correct_predicitons = 0
+
+	for (word, correct) in bigram_list:
+		if word not in model:
+			continue
+		prediction = model[word]
+		sorted(prediction, key=lambda item: item[1],reverse=True)
+		# print word + "-->" + str(prediction)
+
+		#if correct prediction in top 3
+		filtered = list(filter(lambda x: (x[0]==correct), prediction[:3]))
+		# print filtered
+		if len(filtered) > 0:
+			correct_predicitons += 1
+
+	return float(correct_predicitons)/total_bigrams
+
+def create_mapping(model):
+	assoc_map = {}
+
+	for _bigram in model:
+		w1 = _bigram[0][0]
+		w2 = _bigram[0][1]
+		score = _bigram[1]
+		if w1 not in assoc_map:
+			assoc_map[w1] = []
+
+		assoc_map[w1].append((w2,score))
+	return assoc_map
+
 start_time = time()
 validate()
 print "Time elapsed: {:.3f}".format((time()-start_time)) 
